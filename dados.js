732716bfe83337ac33352bec410f675832ca1f0e
Anderson Alves <126093198+anmalves@users.noreply.github.com>
2024-09-06 19:26:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha do Skate, é um fenômeno do esporte brasileiro. É uma skatista brasileira, vice-campeã olímpica nos Jogos Olímpicos de Verão de 2020 em Tóquio, sendo a mais jovem medalhista olímpica brasileira. Em 2024, conquistou o bronze nos Jogos Olímpicos de Paris. ",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        instagram: "https://www.instagram.com/rayssalealsk8/",
+        tags: "skate skatista fada fadinha sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza, potência do judô brasileiro, representa a força e a garra. Beatriz conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris e com isso se tornou a primeira mulher a conquistar uma medalha de ouro Olímpica na história do Esporte Clube Pinheiros.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        instagram: "https://www.instagram.com/_beatrizsouzaar/?hl=en",
+        tags: "judô judo judoca "
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira bicampeã olímpica e a maior medalhista da história do Brasil nas Olimpíadas. Nos Jogos Olímpicos de 2024, Rebeca se tornou a atleta olímpica nacional com mais medalhas nos Jogos, após conquistar o bronze por equipes, a prata no individual geral e no salto, e o ouro no solo. Tornou-se também a primeira brasileira bicampeã olímpica em prova individual.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        instagram: "https://www.instagram.com/rebecarandrade/",
+        tags: "ginástica artística ginastica artistica"
+    }
+];
